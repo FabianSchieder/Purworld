835eb39ab17fe213cb7616fd2a24f5bc894c676b
Fabian Schieder <fabian.schieder@el.htlhl.at>
2024-06-09 18:50:21 +0200

    first commit

---
+++ src/main/java/org/plugin/purpur/Purworld.java
@@ -0,0 +1,17 @@
+package org.plugin.purpur;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Purworld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
