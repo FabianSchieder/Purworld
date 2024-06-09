071986d6c9f5705f96827c99ec8b2210484edd9c
Censatu-12 <chrisi191200@gmail.com>
2024-06-09 18:55:44 +0200

    Update

--- src/main/java/org/plugin/purpur/Purworld.java
+++ src/main/java/org/plugin/purpur/Purworld.java
@@ -5,8 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Purworld extends JavaPlugin {
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
+    public void onEnable()
+    {
 
     }
 
